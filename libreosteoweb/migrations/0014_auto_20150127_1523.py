0802eee350aac09563926ecec6d76677385f933c
Gury Jean-Baptiste <garthylou@users.noreply.github.com>
2015-01-28 16:41:35 +0100

    Update 0014_auto_20150127_1523.py
    Add a default instance for settings when migrating

--- libreosteoweb/migrations/0014_auto_20150127_1523.py
+++ libreosteoweb/migrations/0014_auto_20150127_1523.py
@@ -3,6 +3,24 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+def init_settings(apps, schema_editor):
+    # We can't import the Patient model directly as it may be a newer
+    # version than this migration expects. We use the historical version.
+    OfficeSettings = apps.get_model("libreosteoweb", "OfficeSettings")
+    if len(OfficeSettings.objects.all()) <= 0:
+        officesettings = OfficeSettings()
+        officesettings.invoice_office_header = ''
+        officesettings.office_address_street = ''
+        officesettings.office_address_complement = ''
+        officesettings.office_address_zipcode = ''
+        officesettings.office_address_city = ''
+        officesettings.office_phone = ''
+        officesettings.office_siret = ''
+        officesettings.currency = ''
+        officesettings.invoice_content = ''
+        officesettings.invoice_footer = ''
+        officesettings.invoice_start_sequence = ''
+        officesettings.save()
 
 class Migration(migrations.Migration):
 
@@ -46,4 +64,5 @@ class Migration(migrations.Migration):
             name='office_address_street',
             field=models.CharField(max_length=500, verbose_name='Street', blank=True),
         ),
+        migrations.RunPython(init_settings),
     ]

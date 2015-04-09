6a378395c2858e4419dd33b71a2bbdfcae036045
Jean-Baptiste Gury <garthylou@gmx.com>
2015-04-09 10:53:40 +0200

    Fix the server method to open the webbrowser

--- server.py
+++ server.py
@@ -10,6 +10,38 @@ from cherrypy import _cplogging, _cperror
 from django.conf import settings
 from Libreosteo.wsgi import application
 from django.http import HttpResponseServerError
+import webbrowser
+
+SERVER_PORT = 8080
+
+def _exit(self):
+    """Stop all services and prepare to exit the process."""
+    exitstate = self.state
+    try:
+        self.stop()
+
+        self.state = states.EXITING
+        self.log('Bus EXITING')
+        self.publish('exit')
+        # This isn't strictly necessary, but it's better than seeing
+        # "Waiting for child threads to terminate..." and then nothing.
+        self.log('Bus EXITED')
+    except:
+        # This method is often called asynchronously (whether thread,
+        # signal handler, console handler, or atexit handler), so we
+        # can't just let exceptions propagate out unhandled.
+        # Assume it's been logged and just die.
+        return  # EX_SOFTWARE
+
+    if exitstate == states.STARTING:
+        # exit() was called before start() finished, possibly due to
+        # Ctrl-C because a start listener got stuck. In this case,
+        # we could get stuck in a loop where Ctrl-C never exits the
+        # process, so we just call os.exit here.
+        return
+
+original_exit = cherrypy.process.wspbus.Bus.exit
+cherrypy.process.wspbus.Bus.exit = _exit
  
 class Server(object):
     def __init__(self):
@@ -23,16 +55,25 @@ class Server(object):
         # play nicely with the process bus that is the engine.
         DjangoAppPlugin(cherrypy.engine, self.base_dir).subscribe()
  
-    def run(self):
+    def run(self, callback=None):
         engine = cherrypy.engine
-        cherrypy.config.update({'server.socket_port': 80})
+        cherrypy.config.update({'server.socket_port': SERVER_PORT})
         engine.signal_handler.subscribe()
  
         if hasattr(engine, "console_control_handler"):
             engine.console_control_handler.subscribe()
- 
-        engine.start()
-        engine.block()
+    
+        try :
+            engine.start()
+        except :
+            pass
+
+
+        if callback :
+            callback()
+
+        if engine.state == cherrypy.engine.states.STARTED:
+            engine.block()
  
 class DjangoAppPlugin(plugins.SimplePlugin):
     def __init__(self, bus, base_dir):
@@ -113,6 +154,14 @@ class HTTPLogger(_cplogging.LogManager):
             self.access_log.log(logging.INFO, self.access_log_format % atoms)
         except:
             self.error(traceback=True)
- 
+
+def callback_server_started():
+    """
+    This function is called when the server is started.
+    If an instance is already running, the callback
+    is called too
+    """
+    webbrowser.open("http://localhost:%s/"%(SERVER_PORT), new=2,autoraise=True)
+
 if __name__ == '__main__':
-    Server().run()
+    Server().run(callback_server_started)

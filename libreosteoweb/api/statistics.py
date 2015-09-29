38aec3436a10721a8d74f3247eced21647b26e0a
garthylou <garthylou@gmx.com>
2015-09-29 17:36:32 +0200

    Display sparkline on statistics with history on the office. Need refactoring to be used in production.

--- libreosteoweb/api/statistics.py
+++ libreosteoweb/api/statistics.py
@@ -1,5 +1,6 @@
 from libreosteoweb.models import Patient, Examination
 from datetime import date, timedelta
+import copy
 
 class Statistics(object):
     def __init__(self, *args, **kwargs):
@@ -22,41 +23,170 @@ class Statistics(object):
             'nb_examination':0
         }
 
-    def get_start_of_the_week(self):
-        d = date.today()
+    def get_start_of_the_week(self, current_date=None):
+        if current_date is None :
+            d = date.today()
+        else:
+            d = copy.copy(current_date)
+
         while d.weekday() != 0 :
             d = d - timedelta(days=1)
         return d
 
-    def get_start_of_the_month(self):
-        d = date.today()
+    def get_start_of_the_month(self, current_date=None):
+        if current_date is None :
+            d = date.today()
+        else:
+            d = copy.copy(current_date)
+
         d = d.replace(day=1)
         return d
 
-    def get_start_of_the_year(self):
-        d = date.today()
+    def get_start_of_the_year(self, current_date=None):
+        if current_date is None :
+            d = date.today()
+        else:
+            d = copy.copy(current_date)
+
         d = d.replace(day=1,month=1)
         return d
 
-    def get_week_statistics(self):
-        return self.get_statistics(self.get_start_of_the_week(), self.week_statistics)
+    def get_week_statistics(self, start_date=None, statistics_obj=None):
+        if(start_date is None):
+            start_date = date.today()
+        if statistics_obj is None :
+            statistics_obj = self.week_statistics
+        return self.get_statistics(self.get_start_of_the_week(start_date), start_date, statistics_obj)
 
-    def get_month_statistics(self):
-        return self.get_statistics(self.get_start_of_the_month(), self.month_statistics)
+    def get_month_statistics(self, start_date=None, statistics_obj=None):
+        if(start_date is None):
+            start_date = date.today()
+        if statistics_obj is None :
+            statistics_obj = self.month_statistics
+        return self.get_statistics(self.get_start_of_the_month(start_date), start_date, statistics_obj)
 
-    def get_year_statistics(self):
-        return self.get_statistics(self.get_start_of_the_year(), self.year_statistics)
+    def get_year_statistics(self, start_date=None, statistics_obj=None):
+        if(start_date is None):
+            start_date = date.today()
+        if statistics_obj is None :
+            statistics_obj = self.year_statistics
+        return self.get_statistics(self.get_start_of_the_year(start_date), start_date, statistics_obj)
 
-    def get_statistics(self, limit_date, stats_obj):
-        stats_obj['nb_new_patient'] = Patient.objects.filter(creation_date__gte= limit_date).count()
-        stats_obj['nb_examination'] = Examination.objects.filter(date__gte = limit_date).count()
-        stats_obj['nb_urgent_return'] = Examination.objects.filter(date__gte = limit_date).filter(type = 3).count()
+    def get_statistics(self, start_date, end_date, stats_obj):
+        stats_obj['nb_new_patient'] = Patient.objects.filter(creation_date__gte= start_date, creation_date__lte=end_date).count()
+        stats_obj['nb_examination'] = Examination.objects.filter(date__gte = start_date, date__lte=end_date).count()
+        stats_obj['nb_urgent_return'] = Examination.objects.filter(date__gte = start_date, date__lte=end_date, type = 3).count()
         return stats_obj
+
+    def get_history_week_statistics(self):
+
+        start_date = date.today()
+        week_statistics = {
+            'nb_new_patient':0,
+            'nb_urgent_return':0,
+            'nb_non_paid':0,
+            'nb_examination':0
+        }
+        history_week_statistics = { 
+                'nb_new_patient' : [[],[]],
+                'nb_examination' : [[],[]],
+                'nb_urgent_return': [[],[]]
+                }
+
+        for i in range(1,12) :
+            start_of_the_week = self.get_start_of_the_week(start_date)
+            self.get_week_statistics(start_date, week_statistics)
+            history_week_statistics['nb_new_patient'][0].append("%s - %s" % (start_of_the_week, start_date))
+            history_week_statistics['nb_new_patient'][1].append(week_statistics['nb_new_patient'])
+            history_week_statistics['nb_examination'][0].append("%s - %s" % (start_of_the_week, start_date))
+            history_week_statistics['nb_examination'][1].append(week_statistics['nb_examination'])
+            history_week_statistics['nb_urgent_return'][0].append("%s - %s" % (start_of_the_week, start_date))
+            history_week_statistics['nb_urgent_return'][1].append(week_statistics['nb_urgent_return'])
+            start_date = start_of_the_week - timedelta(days=8)
+
+        history_week_statistics['nb_new_patient'][0]= history_week_statistics['nb_new_patient'][0][::-1]
+        history_week_statistics['nb_new_patient'][1]= history_week_statistics['nb_new_patient'][1][::-1]
+        history_week_statistics['nb_examination'][0]= history_week_statistics['nb_examination'][0][::-1]
+        history_week_statistics['nb_examination'][1]= history_week_statistics['nb_examination'][1][::-1]
+        history_week_statistics['nb_urgent_return'][0]= history_week_statistics['nb_urgent_return'][0][::-1]
+        history_week_statistics['nb_urgent_return'][1]= history_week_statistics['nb_urgent_return'][1][::-1]
+        return history_week_statistics
+
+    def get_history_month_statistics(self):
+        start_date = date.today()
+        month_statistics = {
+            'nb_new_patient':0,
+            'nb_urgent_return':0,
+            'nb_non_paid':0,
+            'nb_examination':0
+        }
+        history_month_statistics = { 
+                'nb_new_patient' : [[],[]],
+                'nb_examination' : [[],[]],
+                'nb_urgent_return': [[],[]]
+                }
+
+        for i in range(1,12) :
+            start_of_the_month = self.get_start_of_the_month(start_date)
+            self.get_month_statistics(start_date, month_statistics)
+            history_month_statistics['nb_new_patient'][0].append("%s - %s" % (start_of_the_month, start_date))
+            history_month_statistics['nb_new_patient'][1].append(month_statistics['nb_new_patient'])
+            history_month_statistics['nb_examination'][0].append("%s - %s" % (start_of_the_month, start_date))
+            history_month_statistics['nb_examination'][1].append(month_statistics['nb_examination'])
+            history_month_statistics['nb_urgent_return'][0].append("%s - %s" % (start_of_the_month, start_date))
+            history_month_statistics['nb_urgent_return'][1].append(month_statistics['nb_urgent_return'])
+            start_date = start_of_the_month - timedelta(days=1)
+
+        history_month_statistics['nb_new_patient'][0]= history_month_statistics['nb_new_patient'][0][::-1]
+        history_month_statistics['nb_new_patient'][1]= history_month_statistics['nb_new_patient'][1][::-1]
+        history_month_statistics['nb_examination'][0]= history_month_statistics['nb_examination'][0][::-1]
+        history_month_statistics['nb_examination'][1]= history_month_statistics['nb_examination'][1][::-1]
+        history_month_statistics['nb_urgent_return'][0]= history_month_statistics['nb_urgent_return'][0][::-1]
+        history_month_statistics['nb_urgent_return'][1]= history_month_statistics['nb_urgent_return'][1][::-1]
+        return history_month_statistics
+
+    def get_history_year_statistics(self):
+        start_date = date.today()
+        year_statistics = {
+            'nb_new_patient':0,
+            'nb_urgent_return':0,
+            'nb_non_paid':0,
+            'nb_examination':0
+        }
+        history_year_statistics = { 
+                'nb_new_patient' : [[],[]],
+                'nb_examination' : [[],[]],
+                'nb_urgent_return': [[],[]]
+                }
+
+        for i in range(1,12) :
+            start_of_the_year = self.get_start_of_the_year(start_date)
+            self.get_year_statistics(start_date, year_statistics)
+            history_year_statistics['nb_new_patient'][0].append("%s - %s" % (start_of_the_year, start_date))
+            history_year_statistics['nb_new_patient'][1].append(year_statistics['nb_new_patient'])
+            history_year_statistics['nb_examination'][0].append("%s - %s" % (start_of_the_year, start_date))
+            history_year_statistics['nb_examination'][1].append(year_statistics['nb_examination'])
+            history_year_statistics['nb_urgent_return'][0].append("%s - %s" % (start_of_the_year, start_date))
+            history_year_statistics['nb_urgent_return'][1].append(year_statistics['nb_urgent_return'])
+            start_date = start_of_the_year - timedelta(days=1)
+
+        history_year_statistics['nb_new_patient'][0]= history_year_statistics['nb_new_patient'][0][::-1]
+        history_year_statistics['nb_new_patient'][1]= history_year_statistics['nb_new_patient'][1][::-1]
+        history_year_statistics['nb_examination'][0]= history_year_statistics['nb_examination'][0][::-1]
+        history_year_statistics['nb_examination'][1]= history_year_statistics['nb_examination'][1][::-1]
+        history_year_statistics['nb_urgent_return'][0]= history_year_statistics['nb_urgent_return'][0][::-1]
+        history_year_statistics['nb_urgent_return'][1]= history_year_statistics['nb_urgent_return'][1][::-1]
+        return history_year_statistics
 
     def compute(self):
         # Do some computation there
         result = {'week':  self.get_week_statistics(),
                   'month': self.get_month_statistics(),
-                  'year' : self.get_year_statistics()
+                  'year' : self.get_year_statistics(),
+                  'history': {
+                    'week': self.get_history_week_statistics(),
+                    'month': self.get_history_month_statistics(),
+                    'year': self.get_history_year_statistics()
+                  }
                 }
         return result
--- libreosteoweb/static/js/app/dashboard.js
+++ libreosteoweb/static/js/app/dashboard.js
@@ -12,22 +12,59 @@ dashboard.factory('DashboardServ', ['$resource',
 dashboard.controller('DashboardCtrl', ['$scope', '$filter', 'growl', 'DashboardServ', 'OfficeEventServ',
     function($scope, $filter, growl, DashboardServ, OfficeEventServ) {
         "use strict";
-        $scope.statistics = DashboardServ.get();
         $scope.selector = 'week';
+
+        var describe_map = function(range_array){
+            var map = {};
+            for(var i = 0; i < range_array.length; i++)
+            {
+                map[i] = range_array[i];
+            }
+            return map;
+        };
+
+        var showObjSparklines = function(obj) {
+            $('.inlinesparkline-newpatient').sparkline(obj.nb_new_patient[1],{
+                type: 'line',
+                tooltipFormat: '{{x:labels}} - {{y}}',
+                tooltipValueLookups: {
+                    labels: $.range_map(describe_map(obj.nb_new_patient[0]))
+                }
+            });
+
+            $('.inlinesparkline-nbexamination').sparkline(obj.nb_examination[1],{
+                type: 'line',
+                tooltipFormat: '{{x:labels}} - {{y}}',
+                tooltipValueLookups: {
+                    labels: $.range_map(describe_map(obj.nb_examination[0]))
+                }
+            });
+            $('.inlinesparkline-urgentreturn').sparkline(obj.nb_urgent_return[1], {
+                type: 'line',
+                tooltipFormat: '{{x:labels}} - {{y}}',
+                tooltipValueLookups: {
+                    labels: $.range_map(describe_map(obj.nb_urgent_return[0]))
+                }
+            });
+        };
+
         $scope.show = function(selector) {
             $scope.selector = selector;
           if(selector == 'week'){
                 $scope.nb_new_patient = $scope.statistics.week.nb_new_patient;
                 $scope.nb_examination = $scope.statistics.week.nb_examination;
                 $scope.nb_urgent_return = $scope.statistics.week.nb_urgent_return;
+                showObjSparklines($scope.statistics.history.week);
           } else if (selector == 'month') {
                 $scope.nb_new_patient = $scope.statistics.month.nb_new_patient;
                 $scope.nb_examination = $scope.statistics.month.nb_examination;
                 $scope.nb_urgent_return = $scope.statistics.month.nb_urgent_return;
+                showObjSparklines($scope.statistics.history.month);
           } else if (selector == 'year') {
                 $scope.nb_new_patient = $scope.statistics.year.nb_new_patient;
                 $scope.nb_examination = $scope.statistics.year.nb_examination;
                 $scope.nb_urgent_return = $scope.statistics.year.nb_urgent_return;
+                showObjSparklines($scope.statistics.history.year);
           }
         };
 
@@ -37,5 +74,6 @@ dashboard.controller('DashboardCtrl', ['$scope', '$filter', 'growl', 'DashboardS
         });
 
         $scope.officeEventLoader = new OfficeEventServ();
+
     }
 ]);

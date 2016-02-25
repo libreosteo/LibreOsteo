8a53b1636fba38e7dea84c5fa1d0a8e79be4f073
Jean-Baptiste G <jbgury@users.noreply.github.com>
2016-02-25 14:24:21 +0100

    Update migrate.sh

--- macos/migrate.sh
+++ macos/migrate.sh
@@ -3,7 +3,7 @@
 /Applications/Libreosteo.app/Contents/MacOS/manage migrate
 myuser=${SUDO_USER:-$USER}
 chown -R ${myuser} "/Users/${myuser}/Library/Application Support/Libreosteo"
-cp /Applications/Libreosteo.app/Contents/Resources/macos/org.libreosteo.macos.Libreosteo.plist "/Library/LaunchAgents/."
-chmod +x "/Library/LaunchAgents/org.libreosteo.macos.Libreosteo.plist"
-sudo launchctl load -w "/Library/LaunchAgents/org.libreosteo.macos.Libreosteo.plist"
+cp /Applications/Libreosteo.app/Contents/Resources/macos/org.libreosteo.macos.Libreosteo.plist "/Library/LaunchDaemons/."
+chmod +x "/Library/LaunchDaemons/org.libreosteo.macos.Libreosteo.plist"
+sudo launchctl load -w "/Library/LaunchDaemons/org.libreosteo.macos.Libreosteo.plist"
 sudo launchctl start org.libreosteo.macos.Libreosteo

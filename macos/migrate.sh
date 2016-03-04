dd906a67b72455a5895ca8c069979c98aac27427
Jean-Baptiste G <jbgury@users.noreply.github.com>
2016-03-04 12:41:14 +0100

    Update migrate.sh
    Revert on the work, use only agents for the user.

--- macos/migrate.sh
+++ macos/migrate.sh
@@ -3,12 +3,9 @@
 /Applications/Libreosteo.app/Contents/MacOS/manage migrate
 myuser=${SUDO_USER:-$USER}
 chown -R ${myuser} "/Users/${myuser}/Library/Application Support/Libreosteo"
-cp /Applications/Libreosteo.app/Contents/Resources/macos/org.libreosteo.macos.Libreosteo.plist "/Library/LaunchDaemons/."
-chmod +x "/Library/LaunchDaemons/org.libreosteo.macos.Libreosteo.plist"
-sudo launchctl load -w "/Library/LaunchDaemons/org.libreosteo.macos.Libreosteo.plist"
+agents_dir="/Users/${myuser}/Library/LaunchAgents"
+mkdir -p ${agents_dir}
+cp /Applications/Libreosteo.app/Contents/Resources/macos/org.libreosteo.macos.Libreosteo.plist ${agents_dir}/.
+chmod +x "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
+sudo launchctl load -w "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
 sudo launchctl start org.libreosteo.macos.Libreosteo
-
-# For previous installation
-if [ -e "/Users/${myuser}/Library/LaunchAgents/org.libreosteo.macos.Libreosteo.plist"]; then
-  rm "/Users/${myuser}/Library/LaunchAgents/org.libreosteo.macos.Libreosteo.plist"
-fi

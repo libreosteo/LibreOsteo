d750ae41af483392e8e26fbb0f6ee1fbba4bf539
Jean Baptiste GURY <garthylou@gmx.com>
2016-03-04 13:14:17 +0100

    Fix the installation script for macos.

--- macos/migrate.sh
+++ macos/migrate.sh
@@ -7,5 +7,6 @@ agents_dir="/Users/${myuser}/Library/LaunchAgents"
 mkdir -p ${agents_dir}
 cp /Applications/Libreosteo.app/Contents/Resources/macos/org.libreosteo.macos.Libreosteo.plist ${agents_dir}/.
 chmod +x "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
-sudo launchctl load -w "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
-sudo launchctl start org.libreosteo.macos.Libreosteo
+chown -R ${myuser} "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
+sudo -u ${myuser} launchctl load -w "${agents_dir}/org.libreosteo.macos.Libreosteo.plist"
+sudo -u ${myuser} launchctl start org.libreosteo.macos.Libreosteo

e267b37c1cbc26388d1e9dbd2bf92bb964a48134
Garthylou Jean-Baptiste Gury <garthylou@gmx.com>
2016-04-29 21:57:37 +0200

    Delete the obsolete config for accessing templates

--- Libreosteo/settings/demonstration.py
+++ Libreosteo/settings/demonstration.py
@@ -6,11 +6,6 @@ DEMONSTRATION = True
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-TEMPLATE_DIRS = (
-    os.path.join(SITE_ROOT, 'templates'),
-    os.path.join(SITE_ROOT, 'static'),
-)
-
 DATABASES['default']['NAME'] = os.path.join(DATA_FOLDER, '../db.sqlite3')
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ('rest_framework.renderers.JSONRenderer',)
 HAYSTACK_CONNECTIONS['default']['PATH'] = os.path.join(DATA_FOLDER, '../whoosh_index')

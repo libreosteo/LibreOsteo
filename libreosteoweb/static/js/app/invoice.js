fa9fd63776b2e453dd6c92c6d80f9a1353925576
Jocelyn Delalande <jocelyn@crapouillou.net>
2018-02-23 16:49:24 +0100

    Offer period selection interface in billing view
    - defaults to current month
    - can select two dates from a Widget
    - can select predefined periods : current month, last year, previous year

--- libreosteoweb/static/js/app/invoice.js
+++ libreosteoweb/static/js/app/invoice.js
@@ -14,8 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with Libreosteo.  If not, see <http://www.gnu.org/licenses/>.
 */
-var invoices = angular.module('loInvoice', ['ngResource']);
-
+var invoices = angular.module('loInvoice', ['ngResource', 'daterangepicker']);
 
 invoices.factory('InvoiceService', ['$resource', function ($resource) {
     "use strict";
@@ -33,9 +32,66 @@ invoices.controller('InvoiceListCtrl', ['$scope','InvoiceService',
     function($scope, InvoiceService) {
         "use strict";
 
+        function getInvoices() {
+            var params = {};
+            InvoiceService.query(
+                {
+                    date__lte: $scope.filters.dateRange.endDate.toISOString(),
+                    date__gte: $scope.filters.dateRange.startDate.toISOString(),
+                },
+                function(result) {
+                    $scope.invoices = result;
+                });
+        }
+
+        /** Prepare pre-defined ranges for daterangepicker
+         *
+         *  http://www.daterangepicker.com/#ex4
+         */
+        function makeMomentRanges() {
+            var ranges = {}
+            ranges[moment().format('MMMM YYYY')] = [
+                moment().startOf('month'),
+                moment().endOf('month')
+            ];
+            ranges[moment().format('YYYY')] = [
+                moment().startOf('year'),
+                moment().endOf('year')
+            ]
+            ranges[moment().subtract(1, 'year').format('YYYY')] = [
+                moment().subtract(1, 'year').startOf('year'),
+                moment().subtract(1, 'year').endOf('year')
+            ];
+            return ranges;
+        }
+
+
         $scope.invoices = [];
-        InvoiceService.query(function(result) {
-            $scope.invoices = result;
-        });
+
+        // Daterangepicker configuration
+        // http://www.daterangepicker.com/#options
+        $scope.daterangePickerOptions = {
+            opens: 'center',
+            ranges: makeMomentRanges(),
+            autoApply: true,
+        }
+
+        $scope.filters = {
+            dateRange: {
+                startDate: moment().startOf('month'),
+                endDate: moment().endOf('month')
+            },
+        };
+
+        // Initial API call
+        getInvoices();
+        // Refresh & filter from API on new date selection
+        $scope.$watch('filters.dateRange', getInvoices);
+
+        $scope.openCalendar = function(){
+            // This is a hack to open the calendar programatically
+            angular.element('input#billing-period').triggerHandler('click');
+        }
+
     }
 ]);

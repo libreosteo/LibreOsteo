fae83f47017eb32d62ff091e6a79c064792d4b87
Joseph Ligier <littel.jo@gmail.com>
2018-03-11 09:33:48 +0100

    Change repo littlejo/libreosteo

---
+++ Docker/build/release/latest/launch-libreosteo.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+local_file='Libreosteo/settings/local.py'
+bin_local_file='Libreosteo/settings/local.pyc'
+tmp_constant_file='/tmp/pre_local.py'
+
+cat <<EOT > $tmp_constant_file
+host = "${sql_host:-db}"
+port = ${sql_port:-5432}
+name = "${sql_name:-postgres}"
+user = "${sql_user:-postgres}"
+EOT
+
+if ! echo "$sql_type" | grep -q 'postgres'
+then
+    rm -f $local_file $bin_local_file
+    cp ${local_file}.sqlite $local_file
+else
+    cat $tmp_constant_file ${local_file}.pg > $local_file
+fi
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8085

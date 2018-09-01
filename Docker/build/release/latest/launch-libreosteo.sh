a2c554112d79b2e08f49a86a720101664da1e9e2
littlejo <joseph.ligier@ymagis.com>
2018-09-01 09:40:34 +0200

    Add django secret key generation for latest release

--- Docker/build/release/latest/launch-libreosteo.sh
+++ Docker/build/release/latest/launch-libreosteo.sh
@@ -3,20 +3,29 @@
 local_file='Libreosteo/settings/local.py'
 bin_local_file='Libreosteo/settings/local.pyc'
 tmp_constant_file='/tmp/pre_local.py'
+tmp2_constant_file='/tmp/pre_local2.py'
+
+KEY=$(python /usr/local/bin/django-secret-key)
 
 cat <<EOT > $tmp_constant_file
-host = "${sql_host:-db}"
-port = ${sql_port:-5432}
-name = "${sql_name:-postgres}"
-user = "${sql_user:-postgres}"
+host = "$sql_host"
+port = $sql_port
+name = "$sql_name"
+user = "$sql_user"
+EOT
+
+cat <<EOT > $tmp2_constant_file
+SECRET_KEY = '$KEY'
+DEBUG = $debug
+TEMPLATE_DEBUG = $debug
 EOT
 
 if ! echo "$sql_type" | grep -q 'postgres'
 then
     rm -f $local_file $bin_local_file
-    cp ${local_file}.sqlite $local_file
+    cat $tmp2_constant_file ${local_file}.sqlite > $local_file
 else
-    cat $tmp_constant_file ${local_file}.pg > $local_file
+    cat $tmp2_constant_file $tmp_constant_file ${local_file}.pg > $local_file
 fi
 
 python manage.py migrate

15e014fe7c523b554ffd96f8b61ed878ff5df256
Garthylou Jean-Baptiste Gury <garthylou@gmx.com>
2019-07-10 16:30:41 +0200

    Fix Resources finder on MacOs

--- Libreosteo/settings/base.py
+++ Libreosteo/settings/base.py
@@ -37,7 +37,7 @@ if getattr(sys, 'frozen', False):
     DATA_FOLDER = SITE_ROOT
     if (getattr(sys, 'frozen', False) == 'macosx_app'):
     	DATA_FOLDER = os.path.join( os.path.join( os.path.join( os.environ['HOME'], 'Library'), 'Application Support' ), 'Libreosteo')
-    	SITE_ROOT = os.path.split(SITE_ROOT)[0]
+    	SITE_ROOT = os.path.join( os.path.split(SITE_ROOT)[0], 'Resources')
     	if not os.path.exists(DATA_FOLDER):
     	    os.makedirs(DATA_FOLDER)
 else:

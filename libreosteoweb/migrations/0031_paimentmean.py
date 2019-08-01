fb8b49c7422f6c99648b68124834e33001e0e059
Jean-Baptiste G <jbgury@users.noreply.github.com>
2019-08-01 21:09:22 +0200

    Issue #190 : migration on postgresql database engine  (#191)
    * issue #190 : fix migration script on postgresql
    
      - migration script for paiment mean does not work on postgresql
      - change the migration script to remove SQL instruction into python
      ORM sequence
    
    * Fix the migration on empty db postgresql

--- libreosteoweb/migrations/0031_paimentmean.py
+++ libreosteoweb/migrations/0031_paimentmean.py
@@ -2,6 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from libreosteoweb.models import PaimentMean
+
+
+def insert_default_paiment_mean(apps, schema_editor):
+    for pm in [('check', 'Chèque', True), ('cash', 'Espèces', True),
+               ('ecard', 'Carte Bancaire', False)]:
+        pm_db = PaimentMean(code=pm[0], text=pm[1], enable=pm[2])
+        pm_db.save()
 
 
 class Migration(migrations.Migration):
@@ -14,13 +22,16 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PaimentMean',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id',
+                 models.AutoField(verbose_name='ID',
+                                  serialize=False,
+                                  auto_created=True,
+                                  primary_key=True)),
                 ('code', models.CharField(max_length=10, verbose_name='Code')),
                 ('text', models.CharField(max_length=50, verbose_name='Text')),
-                ('enable', models.BooleanField(default=True, verbose_name='Enabled')),
+                ('enable',
+                 models.BooleanField(default=True, verbose_name='Enabled')),
             ],
         ),
-        migrations.RunSQL("INSERT INTO libreosteoweb_paimentmean (code, text, enable) VALUES ('check', 'Chèque', 1);"),
-        migrations.RunSQL("INSERT INTO libreosteoweb_paimentmean (code, text, enable) VALUES ('cash', 'Espèces', 1);"),
-        migrations.RunSQL("INSERT INTO libreosteoweb_paimentmean (code, text, enable) VALUES ('ecard', 'Carte Bancaire', 0);")
+        migrations.RunPython(insert_default_paiment_mean)
     ]

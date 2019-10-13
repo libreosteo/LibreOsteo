06a8526445944d6d8f587e1e55d678612dfcb84b
Garthylou Jean-Baptiste Gury <garthylou@gmx.com>
2019-10-13 17:15:02 +0200

    Add support to notify when new version is managed on the central repository

---
+++ libreosteoweb/api/version/version.py
@@ -0,0 +1,20 @@
+import urllib.request
+import json
+import logging
+import libreosteoweb
+from packaging.version import parse
+
+logger = logging.getLogger()
+
+
+def ask_for_new_version():
+    try:
+        logger.info("Ask for new version")
+        contents = urllib.request.urlopen(
+            "http://libreosteo.github.io/api/version")
+        version = json.load(contents)
+        if parse(version['version']) > parse(libreosteoweb.__version__):
+            return (True, version['version'])
+    except Exception as ex:
+        logger.error("Cannot access to version checking", ex)
+    return (False, None)

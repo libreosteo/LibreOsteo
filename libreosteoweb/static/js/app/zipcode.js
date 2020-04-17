f976eb8bb0591ef2988fa50e75e32ddab939f643
Jocelyn Delalande <jocelyn@crapouillou.net>
2020-04-17 18:35:18 +0200

    Allow entering (part of) city name in zipcode field
    For autocomplete sake, that part will be ignored on selection
    
    Ref #216

--- libreosteoweb/static/js/app/zipcode.js
+++ libreosteoweb/static/js/app/zipcode.js
@@ -5,22 +5,43 @@ var zipcode = angular.module('loZipCode', []);
  * Allows to lookup the /zipcode_lookup API to query information about a zip
  * code.
 */
+
+let RE_PARTIAL_ZIPCODE = new RegExp('[0-9]{2,}');
+
+
+/** Extracts the zipcode (or part of zipcode) from a string
+ */
+function extractZipCodePart(val) {
+    let matches = val.match(RE_PARTIAL_ZIPCODE);
+    if (matches) {
+        return matches[0];
+    } else {
+        return null;
+    }
+}
+
+
 zipcode.factory('ZipCodeServ', ['$http',
     function($http) {
         return {
             /** Gives the matching cities matching a zipcode
              *
-             * @param val : a full zipcode
+             * @param val : a string containing a zipcode or part of a zipcode
              * @return an array of zipcode informations, with zero to several
              *   items like `{city: 'ACME', zipcode: '12000'}`
              */
             lookup: function(val) {
-                console.log('Looking up…', val);
-                return $http.get('/zipcode_lookup/zipcode_lookup/'+val).then(
-                    function(response){
-                        console.debug('received', response.data)
-                        return response.data;
-                    });
+                let zipCodePart = extractZipCodePart(val)
+                if (zipCodePart) {
+                    console.log('Looking up…', zipCodePart);
+                    return $http.get('/zipcode_lookup/zipcode_lookup/'+zipCodePart).then(
+                        function(response){
+                            console.debug('received', response.data)
+                            return response.data;
+                        });
+                } else {
+                    console.log('Nothing looking like a zipcode in', val)
+                }
             }
         }
     }

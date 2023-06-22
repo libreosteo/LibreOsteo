63efcd1c15f036731ef71bbb0bf2223de9fb7912
Jean-Baptiste Gury <jean-baptiste.gury@cambiatech.com>
2023-06-22 13:25:36 +0200

    Fix issue with Django last and robotFramework

--- libreosteoweb/models.py
+++ libreosteoweb/models.py
@@ -14,7 +14,7 @@
 # along with LibreOsteo.  If not, see <http://www.gnu.org/licenses/>.
 from django.conf import settings
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from datetime import date
 from django.utils import timezone
 from libreosteoweb.api.utils import enum

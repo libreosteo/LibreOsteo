efbc4b562519d5cbd92fc2153b510592ce394fb5
Jean-Baptiste Gury <jean-baptiste.gury@cambiatech.com>
2023-08-31 23:40:18 +0200

    Revert on previous commit for investigation

--- libreosteoweb/api/serializers.py
+++ libreosteoweb/api/serializers.py
@@ -208,14 +208,14 @@ class ExaminationSerializer(serializers.ModelSerializer):
                                            allow_null=True,
                                            read_only=True)
 
-    date = serializers.DateTimeField(default_timezone=timezone.utc)
+    # date = serializers.DateTimeField(default_timezone=timezone.utc)
 
     class Meta:
         model = Examination
         fields = '__all__'
 
     def validate_date(self, value):
-        if value >= timezone.make_aware(timezone.now(), timezone.utc):
+        if value >= timezone.now():
             raise serializers.ValidationError(
                 _('The examination date is not valid'))
         return value

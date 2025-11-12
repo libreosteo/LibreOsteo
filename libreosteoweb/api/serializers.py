1354c1711e6e33f7c9b9372d60cda1f3bede1682
Jean-Baptiste Gury <jean-baptiste.gury@cambiatech.com>
2025-11-12 18:44:53 +0100

    restore invoice export feature

--- libreosteoweb/api/serializers.py
+++ libreosteoweb/api/serializers.py
@@ -187,7 +187,7 @@ class InvoiceSerializer(WithPkMixin, serializers.ModelSerializer,
     class Meta:
         model = Invoice
         fields = '__all__'
-        depth = 1
+        depth = 0
 
 
 class ExaminationSerializer(serializers.ModelSerializer):
